9677b896b759cb39acf2a2f085a53bc5bcfc7a0c
JulianaMS <juliana.maria@fbter.org.br>
2021-03-30 21:37:01 -0300

    version03 - tst

--- ex01.py
+++ ex01.py
@@ -2,7 +2,7 @@ termo = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 
 decimo = termo + (10 - 1) * razao
-
+print('......PROGRESSÃO ARITMÉTICA......')
 for c in range(termo, decimo + razao, razao):
     print('{}'.format(c), end='-> ')
 print('FIM')
