443eacf7490ca2c15626ed68f9ff7d21f3c600d5
JulianaMS <juliana.maria@fbter.org.br>
2021-03-30 21:26:09 -0300

    Version02 - deleted comments

--- ex01.py
+++ ex01.py
@@ -1,14 +1,6 @@
 termo = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 
-'''valor = termo
-for c in range(termo, 11):
-    if valor == termo:
-        print(valor)
-        valor = valor + razao
-    else:
-        print(valor)
-        valor = valor + razao'''
 decimo = termo + (10 - 1) * razao
 
 for c in range(termo, decimo + razao, razao):
